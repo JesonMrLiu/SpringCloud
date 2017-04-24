8de7079ab72c67e4decafb77cac0cf8ece865cf4
JesonMrLiu <mail.jeson0725@qq.com>
2017-04-24 17:06:50 +0800

    初始导入

---
+++ microservice-consumer-movie/src/main/java/com/spring/cloud/MicroserviceSimpleConsumerMovieApplication.java
@@ -0,0 +1,27 @@
+package com.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MicroserviceSimpleConsumerMovieApplication {
+
+	/**
+	 * 该方法以方法名称作为注解名称，
+	 * <p><b style='color:red'>注意：这个方法的方法名与MovieController的RestTemplate的实例名称一样</b>
+	 * @return
+	 */
+	@Bean
+	public RestTemplate restTmp(){
+		return new RestTemplate();
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSimpleConsumerMovieApplication.class, args);
+	}
+	
+}

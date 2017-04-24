2376544cb4c81cc858aebb02fb1c83f098ea3e14
JesonMrLiu <mail.jeson0725@qq.com>
2017-04-24 17:32:00 +0800

    初始导入

---
+++ microservice-simple-provider-user/src/main/java/com/spring/cloud/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.spring.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.cloud.entity.User;
+import com.spring.cloud.repository.UserRepository;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	private UserRepository userRep;
+	
+	@GetMapping("/simple/{id}")
+	public User findById(@PathVariable Long id){
+		return userRep.findOne(id);
+	}
+	
+}
